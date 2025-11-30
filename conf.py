7b1380926de6b8ab867053729f761a0929baf278
Kristian Rother <kristian.rother@posteo.de>
2025-11-30 22:49:31 +0100

    switch to uv

--- conf.py
+++ conf.py
@@ -1,19 +1,9 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Teaching Programming'
 copyright = '2025, Kristian Rother'
 author = 'Kristian Rother'
 release = '1.0'
 html_title = project
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx_design',
@@ -22,15 +12,16 @@ extensions = [
     'sphinxcontrib.cairosvgconverter',
     ]
 
+
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'de/*']
+exclude_patterns = ['README.md', '_build', 'Thumbs.db', '.DS_Store', 'de/*', 'exercises/*', '.venv/*']
 
 language = 'en'
 
 # ---- Options for HTML output ----
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme_path = ['themes']
+html_theme = 'furo'
 html_static_path = ['_static']
 html_logo = "_static/academis_logo.png"
 html_favicon = "_static/favicon.ico"
@@ -40,13 +31,9 @@ html_css_files = [
 ]
 
 html_theme_options = {
+    "source_branch": "master",
     'logo': 'academis.png',
     'github_user': 'krother',
-    'github_repo': 'training_and_teaching_methods',
-}
-
-# ---- Options for PDF output ----
-
-latex_elements = {
-    'preamble': "\linespread{1.25}",
+    "source_repository": "https://github.com/krother/training_and_teaching_methods",
+    "source_directory": "",
 }
